6bd634201febb81d4b57463d0499d366faf35f23
turboleander <53143665+turboleander@users.noreply.github.com>
2026-03-27 10:57:31 +0700

    week11

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,12 +5,17 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
+    public Transform focalPoint;
+
+    public bool hasPowerUp;
 
     private Rigidbody rb;
 
     private InputAction moveAction;
     private InputAction smashAction;
     private InputAction breakAction;
+
+    private Coroutine powerUpRoutine;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -25,6 +30,46 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        var move = moveAction.ReadValue<Vector2>();
+        rb.AddForce(move.y * speed * focalPoint.forward);
+        if (breakAction.IsPressed())
+        {
+            rb.linearVelocity = Vector3.zero;
+        }
+    }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            if (hasPowerUp)
+            {
+                var enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+                //var v = enemyRb.linearVelocity;
+                //v.Normalize();
+                var dir = enemyRb.transform.position - transform.position;
+                dir.Normalize();
+                enemyRb.AddForce(dir * 10, ForceMode.Impulse);
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+            if(powerUpRoutine != null)
+            {
+                StopCoroutine(powerUpRoutine);
+            }
+            powerUpRoutine = StartCoroutine(PowerUpCooldown());
+        }
+    }
+
+    IEnumerator PowerUpCooldown()
+    {
+        yield return new WaitForSeconds(10f);
+        hasPowerUp = false;
     }
 }
